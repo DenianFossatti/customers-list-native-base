a909dd70092a83fd5283f8eef8f3c6cd756e3c69
Denian Camilotti Fossatti <denian.fossatti@gmail.com>
2021-11-21 19:04:33 -0300

    fix: react-navigation types

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,15 +1,23 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import CustomerDetails from '../screens/CustomerDetails';
+
 import CustomersList from '../screens/CustomersList';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  CustomersList: undefined;
+  CustomerDetails: { customerId: number };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="CustomersList">
         <Stack.Screen name="CustomersList" component={CustomersList} />
+        <Stack.Screen name="CustomerDetails" component={CustomerDetails} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ types/navigation.d.ts
@@ -0,0 +1,8 @@
+import { RootStackParamList } from '../src/router/AppRouter';
+
+declare global {
+  namespace ReactNavigation {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface RootParamList extends RootStackParamList {}
+  }
+}
