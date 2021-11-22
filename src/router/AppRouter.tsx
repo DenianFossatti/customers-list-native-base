2799b22805c45b88660fe80bd1ed35de7458d10e
Denian Camilotti Fossatti <denian.fossatti@gmail.com>
2021-11-22 02:58:21 -0300

    feat: change header style

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useTheme } from 'native-base';
 
 import CustomerDetails from '../screens/CustomerDetails';
 
@@ -13,10 +14,22 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
+  const theme = useTheme();
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="CustomersList">
-        <Stack.Screen name="CustomersList" component={CustomersList} />
+      <Stack.Navigator
+        initialRouteName="CustomersList"
+        screenOptions={{
+          headerStyle: { backgroundColor: theme.colors.blue['500'] },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerTitleAlign: 'center',
+        }}
+      >
+        <Stack.Screen name="CustomersList" component={CustomersList} options={{ title: 'Customers' }} />
         <Stack.Screen name="CustomerDetails" component={CustomerDetails} />
       </Stack.Navigator>
     </NavigationContainer>
